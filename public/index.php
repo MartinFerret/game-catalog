5b473d8fbc5632c9d6e49465d52775d6fef483b9
Vityy <ygensoftvd@gmail.com>
2026-01-09 09:35:03 +0100

    Merge bill branch in main

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@ use Controller\PingApiController;
 use Controller\ProjectController;
 use Controller\ClientController;
 use Controller\AbsenceController;
+use Controller\SalarieApiController;
 use Controller\TacheApiController;
 use Core\Cors;
 use Core\Database;
@@ -14,6 +15,7 @@ use Core\Router;
 
 use Repository\ProjectsRepository;
 use Repository\ClientsRepository;
+use Repository\SalarieRepository;
 use Repository\TacheRepository;
 
 session_start();
@@ -22,6 +24,7 @@ $registerProjectRoutes = require __DIR__ . '/../config/projectRoutes.php';
 $registerClientsRoutes = require __DIR__ . '/../config/clientRoutes.php';
 $registerAbsenceRoutes = require __DIR__ . '/../config/absenceRoutes.php';
 $registerTachesRoutes = require __DIR__ . '/../config/tachesRoutes.php';
+$registerSalarieRoutes = require __DIR__ . '/../config/salarieRoutes.php';
 $config = require_once __DIR__ . '/../config/db.php';
 
 Cors::Handle();
@@ -34,15 +37,18 @@ $router = new Router();
 $projectRepository = new ProjectsRepository(Database::makePdo($config['db']));
 $clientRepository = new ClientsRepository(Database::makePdo($config['db']));
 $tacheRepository = new TacheRepository(Database::makePdo($config['db']));
+$salarieRepository = new SalarieRepository(Database::makePdo($config['db']));
 
+$pingApiController = new PingApiController();
 $projectController = new ProjectController($response, $projectRepository, $session, $request);
 $clientController = new ClientController($response, $clientRepository, $session, $request);
 $absenceController = new AbsenceController(Database::makePdo($config['db']));
 $tacheApiController = new TacheApiController($response, $tacheRepository, $request);
-$pingApiController = new PingApiController();
+$salarieController = new SalarieApiController($response, $salarieRepository, $session, $request);
 
 $registerProjectRoutes($router, $projectController, $pingApiController, $projectRepository);
 $registerClientsRoutes($router, $clientController, $pingApiController, $clientRepository);
 $registerAbsenceRoutes($router, $pingApiController, $absenceController);
 $registerTachesRoutes($router, $pingApiController, $tacheApiController, $tacheRepository);
+$registerSalarieRoutes($router, $salarieController);
 $router->dispatch($request, $response);
